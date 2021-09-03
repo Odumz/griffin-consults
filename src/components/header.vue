b114dbe5fc9f7370c0231da6d1066b33620f9f08
Odumz <odumz20@gmail.com>
2021-09-03 03:32:35 +0100

    header updated and responsive

--- src/components/header.vue
+++ src/components/header.vue
@@ -26,13 +26,13 @@
               <PopoverPanel class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
                 <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                   <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                    <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                    <a v-for="item in solutions" :key="item.name" :href="item.href" class="-m-3 p-3 flex rounded-lg hover:bg-gray-50">
                       <Icon :icon="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
                       <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
+                        <p class="text-base flex font-medium text-gray-900">
                           {{ item.name }}
                         </p>
-                        <p class="mt-1 text-sm text-gray-500">
+                        <p class="mt-1 text-sm text-left text-gray-500">
                           {{ item.description }}
                         </p>
                       </div>
@@ -71,10 +71,10 @@
                     <a v-for="item in resources" :key="item.name" :href="item.href" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
                       <Icon :icon="item.icon" class="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
                       <div class="ml-4">
-                        <p class="text-base font-medium text-gray-900">
+                        <p class="text-base flex font-medium text-gray-900">
                           {{ item.name }}
                         </p>
-                        <p class="mt-1 text-sm text-gray-500">
+                        <p class="mt-1 text-sm text-left text-gray-500">
                           {{ item.description }}
                         </p>
                       </div>
@@ -205,7 +205,7 @@ const solutions = [
     name: 'Automations',
     description: 'Build strategic funnels that will drive your customers to convert',
     href: '#',
-    icon: 'heroicons-outline:refresh" />'
+    icon: 'heroicons-outline:refresh'
   }
 ]
 const callsToAction = [
