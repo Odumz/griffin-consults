937f73e9749cd84272135f2cc0a231ea8d809c21
Odumz <odumz20@gmail.com>
2021-09-07 00:37:31 +0100

    footer component updated

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
   </div> -->
   <header-nav class="z-index" />
   <router-view class=""/>
+  <footer-nav />
 </template>
 
 <script>
 import { defineComponent } from '@vue/runtime-core'
 import headerNav from './components/header.vue'
+import footerNav from './components/footer.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    headerNav
+    headerNav,
+    footerNav
   }
 })
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="footer_widgets" class="md:flex p-3">
+    <div id="about" class="md:w-2/5 md:flex border border-purple-600 px-3 py-6">
+      <div id="logo" class="md:w-1/3 flex justify-center items-center pr-4 pb-4">
+        <img class="h-8 w-auto sm:h-10" src="../assets/logo.png" alt="GriffinTech" />
+      </div>
+      <div id="about_text" class="md:w-2/3 flex text-left">
+        <p>This is one of the most interesting projects I've had the opportunity of working on. It really got me inpsired and I was glad I jumped on it. </p>
+      </div>
+    </div>
+    <div id="quicklinks" class="md:w-1/5 border border-purple-600 px-3 py-6">
+      <ul>
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+      </ul>
+    </div>
+    <div id="contact" class="md:w-2/5 border border-purple-600 px-3 py-6">
+      <div id="map"></div>
+      <div id="contact_info">
+        <p>phone</p>
+        <p>phone</p>
+        <p>phone</p>
+      </div>
+    </div>
+  </div>
+  <div id="copyright">
+    <p class="flex justify-center py-4 text-skin-cerebro-about-purple">&copy; GriffinTech 2021</p>
+  </div>
+</template>
