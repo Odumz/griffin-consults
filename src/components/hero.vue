6ffa734e5ad000ff813497d0ef45d0ea33f8df9d
Odumz <odumz20@gmail.com>
2021-10-01 15:47:44 +0100

    gsap installed...not yet working

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -56,17 +56,17 @@
           </transition>
         </Popover>
 
-        <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-          <div class="sm:text-center lg:text-left">
-            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <main class="reveal mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <div class="sm:text-center lg:text-left reveal">
+            <h1 class="reveal fromLeft text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span class="block xl:inline">Data to enrich your</span>
               {{ ' ' }}
               <span class="block text-indigo-600 xl:inline">online business</span>
             </h1>
-            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            <p ref="text" class="reveal mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
             </p>
-            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div ref="buttons" class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div class="rounded-md shadow">
                 <a href="/register" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                   Get started
@@ -82,7 +82,7 @@
         </main>
       </div>
     </div>
-    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <div class="reveal fromRight lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
     </div>
   </div>
@@ -91,14 +91,16 @@
 <script lang="ts">
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
 import { Icon } from '@iconify/vue'
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' }
-]
+// const navigation = [
+//   { name: 'Product', href: '#' },
+//   { name: 'Features', href: '#' },
+//   { name: 'Marketplace', href: '#' },
+//   { name: 'Company', href: '#' }
+// ]
 
 export default defineComponent({
   components: {
@@ -108,6 +110,64 @@ export default defineComponent({
     Icon
   },
   setup () {
+    onMounted(() => {
+      gsap.registerPlugin(ScrollTrigger)
+      animate()
+    })
+
+    const navigation = ref([
+      { name: 'Product', href: '#' },
+      { name: 'Features', href: '#' },
+      { name: 'Marketplace', href: '#' },
+      { name: 'Company', href: '#' }
+    ])
+
+    const animateFrom = (elem: any, direction: number) => {
+      let x = 0
+      let y = direction * 100
+
+      if (elem.classList.contains('fromLeft')) {
+        x = -100
+        y = 0
+      } else if (elem.classList.contains('fromRight')) {
+        x = 100
+        y = 0
+      }
+      elem.style.transform = `translate(${x}px, ${y}px)`
+      elem.style.opacity = '0'
+      gsap.fromTo(elem, {
+        x,
+        y,
+        autoAlpha: 0
+      }, {
+        duration: 3.25,
+        x: 0,
+        y: 0,
+        autoAlpha: 1,
+        ease: 'expo',
+        overwrite: 'auto'
+      })
+    }
+
+    const hide = (elem: string): void => {
+      gsap.set(elem, { autoAlpha: 0 })
+    }
+
+    const animate = (): void => {
+      gsap.utils.toArray('reveal').forEach((elem: any) => {
+        hide(elem)
+
+        // const header = refs.header
+        // const text = this.$refs.firstText
+
+        ScrollTrigger.create({
+          trigger: elem,
+          onEnter: () => { animateFrom(elem, 1) },
+          onEnterBack: () => { animateFrom(elem, -1) },
+          onLeave: () => { hide(elem) }
+        })
+      })
+    }
     return {
       navigation
     }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './main.css'
+import { gsap } from 'gsap'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(gsap).mount('#app')
