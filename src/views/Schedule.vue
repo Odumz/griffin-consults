68c07f57eea391c2a40772dc857c7a45bcfbec20
Odumz <odumz20@gmail.com>
2021-11-20 01:10:23 +0100

    schedule and video call pages added

---
+++ src/views/Schedule.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex">
+    <!-- image & form-tracker -->
+    <div></div>
+    <!-- form -->
+    <div>
+      <!-- tracker image -->
+      <div></div>
+      <!-- heading -->
+      <div>
+        <h1>Schedule a conference call</h1>
+        <p>Enter the necessary information below to get a link.</p>
+      </div>
+      <!-- form -->
+      <form action="">
+        <div>
+          <p>Title</p>
+          <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Let's have dinner">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
---
+++ src/views/VideoCall.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex my-5">
+    <!-- video-conference -->
+    <div></div>
+    <!-- notes & chat -->
+    <div>
+      <!-- notes -->
+      <div></div>
+      <!-- chat -->
+      <div></div>
+    </div>
+  </div>
+  <!-- chat input -->
+  <div class="">
+    <div class="flex items-center md:shadow-lg justify-center pb-6 md:py-0 md:border-b-8 border-indigo-900">
+      <form>
+          <div class="flex flex-col rounded-lg min-w-max overflow-hidden sm:flex-row">
+              <input class="py-3 px-4 bg-gray-200 text-gray-800 border-gray-300 border-2 outline-none placeholder-gray-500 focus:bg-gray-100" type="text" name="email" placeholder="Enter your email">
+              <button class="py-3 px-4 bg-indigo-900 text-gray-100 font-semibold uppercase hover:bg-gray-600">Send</button>
+          </div>
+      </form>
+    </div>
+  </div>
+</template>
