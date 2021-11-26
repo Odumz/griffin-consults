063cec25064aa59158445111000831c8fa7faf32
Odumz <odumz20@gmail.com>
2021-11-26 01:39:47 +0100

    faq page added

---
+++ src/views/FAQ.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="px-8 sm:px-20">
+    <div class="title reveal fromBottom">
+      <h1 class="text-4xl text-indigo-900 py-5 mt-3 font-semibold">Frequently Asked Questions</h1>
+      <p>Check out our frequently asked questions for useful hints on how to get the best out of Clicka or just for answers to unending questions.</p>
+    </div>
+    <div class="image reveal fromBottom">
+      <img src="https://res.cloudinary.com/griffintech/image/upload/v1637845077/clicka/faq-main_-compressed_nzbofz.png" alt="FAQ">
+    </div>
+    <div class="content">
+      <details>
+        <summary class="text-indigo-900 text-xl font-semibold">What is the conference?</summary>
+        <p class="text-lg">
+          The conference is a one-day event that will be held in the heart of the city of Bhringraj, in the state of Gujarat. The conference will be held on the first day of the conference.
+        </p>
+      </details>
+      <details>
+        <summary class="text-indigo-900 text-xl font-semibold">What is the conference?</summary>
+        <p class="text-lg">
+          The conference is a one-day event that will be held in the heart of the city of Bhringraj, in the state of Gujarat. The conference will be held on the first day of the conference.
+        </p>
+      </details>
+      <details>
+        <summary class="text-indigo-900 text-xl font-semibold">What is the conference?</summary>
+        <p class="text-lg">
+          The conference is a one-day event that will be held in the heart of the city of Bhringraj, in the state of Gujarat. The conference will be held on the first day of the conference.
+        </p>
+      </details>
+      <img src="https://res.cloudinary.com/griffintech/image/upload/v1637846826/clicka/questions-comp_d6ruje.png" alt="" class="w-2/9 relative left-2/4">
+    </div>
+  </div>
+</template>
+
+<style>
+  .title {
+  }
+
+  .image {
+    @apply w-3/7 flex justify-center items-center mx-auto;
+  }
+
+  .content {
+    @apply flex flex-col justify-center items-center my-10 mx-auto w-full;
+  }
+</style>
