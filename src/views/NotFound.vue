c289c129e892878f44c203b444cb167c3cf3612b
Odumz <odumz20@gmail.com>
2021-11-27 14:05:39 +0100

    error 404 page added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center bg-gray-50">
+    <img src="https://res.cloudinary.com/griffintech/image/upload/v1638012860/clicka/404_nrs4hp.jpg" alt="" class="h-screen">
+  </div>
+</template>
