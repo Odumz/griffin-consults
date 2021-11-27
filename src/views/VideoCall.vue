950b092986039fc81a34fa2ad18c0a3d44a9782e
Odumz <odumz20@gmail.com>
2021-11-27 11:51:09 +0100

    clicka conference done; responsiveness completed

--- src/views/VideoCall.vue
+++ src/views/VideoCall.vue
@@ -1,46 +1,32 @@
 <template>
-  <div class="flex pb-32 px-8 sm:px-20 pt-10 mx-auto justify-center">
+  <div class="md:flex pt-14 px-20 justify-around items-end shadow-lg pb-5">
+    <div class="flex flex-col items-start">
+      <h3 class="text-sm text-indigo-800">Clicka Conference</h3>
+      <h1 class="text-3xl text-indigo-900 font-semibold">Big Title here</h1>
+    </div>
+    <div class="flex">
+      <div class="flex items-center text-indigo-900 text-xl">
+        <Icon icon="bytesize:clock" class="text-3xl" />
+        <p class="px-2">10/12/2021 10:00</p>
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col lg:flex-row pb-10 gap-5 px-8 my-10 sm:px-20 pt-10 mx-auto justify-center">
     <!-- video-conference -->
-    <div class="flex justify-center items-center w-3/5">
-      <div class="h-96 w-full mx-5 border-2">
-        <!-- <QuillEditor
-          class=" rounded-b"
-          theme="snow"
-          toolbar="full"
-          v-model="quillEditor"
-          :options="quillOptions"
-          @blur="onBlur"
-          @focus="onFocus"
-          @ready="onReady"
-          @text-change="onTextChange"
-          @selection-change="onSelectionChange"
-          @change="onChange"
-          @selection-created="onSelectionCreated"
-          @selection-removed="onSelectionRemoved"
-          @selection-updated="onSelectionUpdated"
-          @selection-readied="onSelectionReadied"
-          @selection-unreadied="onSelectionUnreadied"
-          @selection-readied-change="onSelectionReadiedChange"
-          @selection-unreadied-change="onSelectionUnreadiedChange"
-          @selection-readied-change-cancel="onSelectionReadiedChangeCancel"
-          @selection-unreadied-change-cancel="onSelectionUnreadiedChangeCancel"
-          @selection-readied-change-error="onSelectionReadiedChangeError"
-          @selection-unreadied-change-error="onSelectionUnreadiedChangeError"
-          @selection-readied-change-success="onSelectionReadiedChangeSuccess"
-          @selection-unreadied-change-success="onSelectionUnreadiedChangeSuccess"
-          @selection-readied-change-success-cancel="onSelectionReadiedChangeSuccessCancel"
-          @selection-unreadied-change-success-cancel="onSelectionUnreadiedChangeSuccessCancel"
-          /> -->
+    <div class="md:flex justify-center items-center lg:w-3/5 shadow-lg">
+      <div class="h-auto w-full">
+        video call goes here
       </div>
     </div>
     <!-- notes & chat -->
-    <div class="flex justify-center w-2/5">
+    <div class="flex flex-col gap-2 lg:w-2/5">
       <!-- notes -->
-      <div class="">
-        <div class="h-48">
+      <div class="md:w-full shadow-lg rounded">
+        <h1 class="text-indigo-900 p-2 text-left">Notes</h1>
+        <div class="h-32">
           <QuillEditor
             class=" rounded-b"
-            theme="snow"
+            theme="bubble"
             toolbar="full"
             v-model:content="notes"
             @ready="test"
@@ -49,18 +35,16 @@
         </div>
       </div>
       <!-- chat -->
-      <div class="border-t-2 "></div>
+      <div v-if="chatOpen" class="shadow-lg border-2 mt-2 px-4 py-6 rounded md:w-full h-52">ji</div>
     </div>
   </div>
   <!-- chat input -->
-  <div class="my-4">
-    <div class="flex items-center md:shadow-lg justify-center pb-6 md:py-0 md:border-b-8 border-indigo-900">
-      <!-- <input type="text" class="p-3 w-11/12 bg-gray-600"> -->
-      <!-- <form class=""> -->
-          <div class="flex flex-col rounded-lg overflow-hidden sm:flex-row">
-            <input class="py-3 px-4 bg-gray-200 text-gray-800 border-gray-300 border-2 outline-none placeholder-gray-500 focus:bg-gray-100" type="text" name="message" placeholder="type here ...">
-            <button class="py-3 px-4 bg-indigo-900 text-gray-100 font-semibold uppercase hover:bg-gray-600">Send</button>
-          </div>
+  <div v-if="chatOpen" class="my-4 lg:pt-0">
+    <div class="items-center md:shadow-lg justify-center pb-6 md:py-0">
+      <div class="flex flex-col mx-8 sm:mx-20 rounded-lg overflow-hidden sm:flex-row pb-10">
+        <input class="rounded-lg py-2 sm:w-11/12 px-4 bg-gray-200 text-gray-800 border-gray-300 border-2 outline-none placeholder-gray-500 focus:bg-gray-100" type="text" name="message" placeholder="type here ...">
+        <button class="py-2 px-4 rounded-lg bg-indigo-900 lg:rounded-tr-lg text-gray-100 font-semibold uppercase hover:bg-gray-600">Send</button>
+      </div>
       <!-- </form> -->
     </div>
   </div>
@@ -68,11 +52,11 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue'
+import { Icon } from '@iconify/vue'
 
 export default defineComponent({
   name: 'Video Call',
-  components: {
-  },
+  components: { Icon },
   setup () {
     onMounted(async () => {
       console.log('mounted')
@@ -94,12 +78,14 @@ export default defineComponent({
       // console.log(notes)
     }
 
+    const chatOpen = ref(true)
+
     const newText = () => {
       console.log('new notes here', notes.value)
     }
 
     return {
-      test, notes, newText
+      test, notes, newText, chatOpen
     }
   }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,8 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      '2xl': '1536px'
+      '2xl': '1536px',
+      mg: '803px'
     },
     zIndex: {
       0: 0,
